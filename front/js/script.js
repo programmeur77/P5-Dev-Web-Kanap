3786020a1746d3e3427e1efdc93b63d471c71ab2
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-17 22:07:55 +0100

    Changed items.innerHTML with items.instertAdjecentHTML to get all products displayed

--- front/js/script.js
+++ front/js/script.js
@@ -29,12 +29,14 @@ function getAllProducts(url) {
 function displayContent(objectToDisplay) {
 
     for (let product of objectToDisplay) {
-        items.innerHTML = `<a href="./product.html?id=${product._id}">
-                                      <article>
-                                          <img src="${product.imageUrl}" alt="${product.altTxt}" />
-                                          <h3 class="productName">${product.name}</h3>
-                                          <p class="productDescription">${product.description}</p>
-                                      </article>
-                                  </a>`
+        items.insertAdjacentHTML('beforeend', 
+                                    `<a href="./product.html?id=${product._id}">
+                                        <article>
+                                            <img src="${product.imageUrl}" alt="${product.altTxt}" />
+                                            <h3 class="productName">${product.name}</h3>
+                                            <p class="productDescription">${product.description}</p>
+                                        </article>
+                                    </a>`
+                                )
     }
 }
