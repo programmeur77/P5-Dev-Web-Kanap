ba789ed5aafe76a39660da247a6fc86870906359
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-19 14:47:01 +0100

    Added function to display correct product + added function reporting to localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,38 +1,52 @@
 const params = new URLSearchParams(window.location.search),
-      productId = params.get('id');
+  productId = params.get('id');
 
 const imgPlace = document.querySelector('article .item__img'),
-      productTitle = document.getElementById('title'),
-      productPrice = document.getElementById('price'),
-      productDescription = document.getElementById('description'),
-      productColors = document.getElementById('colors');
+  productTitle = document.getElementById('title'),
+  productPrice = document.getElementById('price'),
+  productDescription = document.getElementById('description'),
+  productColors = document.getElementById('colors'),
+  productQuantity = document.getElementById('quantity'),
+  submit = document.getElementById('addToCart');
 
-window.addEventListener('load', function() {
-    fetchProduct(productId);
-})
+let cart = {
+  id: this.id,
+  color: this.color,
+  quantity: this.quantity
+};
+
+const cartContent = [];
+
+window.addEventListener('load', function () {
+  fetchProduct(productId);
+});
+
+submit.addEventListener('click', function () {
+  cart = {
+    id: productId,
+    color: productColors.value,
+    quantity: productQuantity.value
+  };
+
+  isSetStorage(cart);
+});
 
 /**
  * Fetch info about a specific product accrording to product ID
- * @param { String } productId 
+ * @param { String } productId
  */
 
 function fetchProduct(productId) {
-    fetch(`http://localhost:3000/api/products/${productId}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            document.title = `${data.name}`;
-            imgPlace.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}" />`;
-            productTitle.innerHTML = `${data.name}`;
-            productPrice.innerHTML = `${data.price}`;
-            productDescription.innerHTML = data.description;
-            colorOptions(data.colors);
-        })
-        .catch((error) => {
-            console.error(error);
-        })
+  fetch(`http://localhost:3000/api/products/${productId}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      displayData(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 /**
@@ -41,7 +55,73 @@ function fetchProduct(productId) {
  */
 
 function colorOptions(optionArray) {
-    for(let color of optionArray) {
-        productColors.insertAdjacentHTML('beforeend', `<option value="${color}">${color}</option>`);
+  for (let color of optionArray) {
+    productColors.insertAdjacentHTML(
+      'beforeend',
+      `<option value="${color}">${color}</option>`
+    );
+  }
+}
+
+/**
+ * Displays data in the form
+ * @param { Object } data
+ */
+function displayData(data) {
+  document.title = `${data.name}`;
+  imgPlace.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}" />`;
+  productTitle.innerHTML = `${data.name}`;
+  productPrice.innerHTML = `${data.price}`;
+  productDescription.innerHTML = data.description;
+  colorOptions(data.colors);
+}
+
+/**
+ * Stores data in local memory
+ * @param { Array } dataToStore
+ */
+function setLocalStorage(dataToStore) {
+  localStorage.setItem('totalCart', JSON.stringify(dataToStore));
+}
+
+/**
+ * Get a localStorage item and turns it into an JSON/Object format
+ * @param { String } dataToGet
+ * @param { Array } [content]
+ * @returns content in JSON/Object format
+ */
+function getLocalStorage(dataToGet) {
+  return JSON.parse(localStorage.getItem(dataToGet));
+}
+
+/**
+ * Checks if a local storage exists for actual cart
+ * @param { Object } cart
+ */
+function isSetStorage(cart) {
+  if (!localStorage.getItem('totalCart')) {
+    cartContent.push(cart);
+    setLocalStorage(cartContent);
+  } else {
+    var storageIndex = findStorageContent();
+    if (storageIndex == -1) {
+      cartContent.push(cart);
+      setLocalStorage(cartContent);
+    } else {
+      modifyQuantity(cart, storageIndex);
     }
+  }
+}
+
+/**
+ * Check if there's already an article with the same id AND color in localStorage items
+ * @returns index number if found or -1 if nothing found
+ */
+function findStorageContent() {
+  var cart = getLocalStorage('totalCart');
+  var index = cart.findIndex(
+    (elements) =>
+      elements.id == productId && elements.color == productColors.value
+  );
+  return index;
 }
