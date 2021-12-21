9b166be3cef94e66c7de3aec6f29790807d2270e
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-21 23:48:50 +0100

    Coded load event and fetch function for cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,69 +1,23 @@
+const cartItems = document.querySelector('.cart #cart__items'),
+  article = document.querySelector('article');
+
+const cartContent = getLocalStorage('totalCart');
+
 window.addEventListener('load', function () {
-  getProductIds();
+  getProducts(cartContent);
 });
 
-/**
- * Stores data in local memory
- * @param { Array } dataToStore
- */
-function setLocalStorage(dataToStore) {
-  localStorage.setItem('totalCart', JSON.stringify(dataToStore));
-}
-
-/**
- * Get a localStorage item and turns it into an JSON/Object format
- * @param { String } dataToGet
- * @param { Array } [content]
- * @returns content in JSON/Object format
- */
-function getLocalStorage(dataToGet) {
-  console.log(localStorage);
-  return JSON.parse(localStorage.getItem(dataToGet));
-}
-
-/**
- * Checks if a local storage exists for specified LocalStorage key
- * @param { String } key
- * @returns { Boolean }
- */
-function isSetStorage(key) {
-  if (localStorage.getItem(key)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/**
- * Check if there's already an article with the same id AND color in localStorage items
- * @returns { Number } index number if found or -1 if nothing found
- */
-function findStorageContent() {
-  var cart = getLocalStorage('totalCart');
-  var index = cart.findIndex(
-    (elements) =>
-      elements.id == productId && elements.color == productColors.value
-  );
-  return index;
-}
-
-/**
- * Convert quantity strings to numbers and update it in the correct array entry
- * @param { Number } storageIndex index of the quantity entry that must be updated
- * @returns
- */
-function modifyQuantity(storageIndex) {
-  let cart = getLocalStorage('totalCart');
-
-  let currentQuantity = parseInt(cart[storageIndex].quantity);
-  let quantityToAdd = parseInt(productQuantity.value);
-
-  cart[storageIndex].quantity = currentQuantity + quantityToAdd;
-
-  return cart;
-}
-
-function getProductIds() {
-  let cart = getLocalStorage('totalCart');
-  console.log(cart);
+function getProducts(products) {
+  products.map(function (element) {
+    fetch(`http://localhost:3000/api/products/${element.id}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        displayContent(data, element);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
 }
