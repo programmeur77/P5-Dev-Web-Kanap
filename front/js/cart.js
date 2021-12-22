c22fcc00d0296c44cff749521fae67201bb4479e
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-23 00:33:26 +0100

    Trying to trigger function from window.load instead of from getProducts function

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,23 +1,102 @@
 const cartItems = document.querySelector('.cart #cart__items'),
-  article = document.querySelector('article');
+  article = document.getElementsByTagName('article'),
+  quantity = document.getElementsByClassName('itemQuantity');
 
 const cartContent = getLocalStorage('totalCart');
 
+let array = [],
+  dataArray = [],
+  dataSetArray = [];
+
 window.addEventListener('load', function () {
-  getProducts(cartContent);
+  let data;
+  cartContent.map((element) => {
+    data = getProductsToBuy(element.id);
+    console.log(data);
+  });
 });
 
-function getProducts(products) {
-  products.map(function (element) {
-    fetch(`http://localhost:3000/api/products/${element.id}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        displayContent(data, element);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+function getProductsToBuy(elementId) {
+  fetch(`http://localhost:3000/api/products/${elementId}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      return jsonData;
+    });
+}
+
+// function getProducts(arrayToGet) {
+//   arrayToGet.map((element) => {
+//     fetch(`http://localhost:3000/api/products/${element.id}`)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         displayContent(data, element);
+//         convertCollectionToArray(article);
+//         getDataset(array);
+//         addListenerOnCollection(quantity, 'change');
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   });
+// }
+
+function convertCollectionToArray(htmlCollection) {
+  array = Array.from(htmlCollection);
+}
+
+function getDataset(array) {
+  array.map((element) => {
+    dataSetArray.push(element.dataset);
   });
+}
+
+function addListenerOnCollection(collection, eventToListenTo) {
+  for (let index of collection) {
+    collection.item(index).addEventListener(eventToListenTo, function () {
+      let newCartQuantityArray = updateCartQuantity(cartContent, dataSetArray);
+      localStorage.removeItem('totalCart');
+      //TODO Remplace former Array with the quantity modified one
+    });
+  }
+}
+
+function updateCartQuantity(storageContentArray, productDatasetArray) {
+  let newCart = {};
+  let newCartQuantityArray = [];
+  let currentQuantity = parseInt(getQuantity());
+  for (product of storageContentArray) {
+    for (element of productDatasetArray) {
+      if (
+        product.id === element.id &&
+        product.color === element.color &&
+        product.quantity != currentQuantity
+      ) {
+        newCart = {
+          id: product.id,
+          color: product.color,
+          quantity: currentQuantity
+        };
+      } else {
+        newCart = {
+          id: product.id,
+          color: product.color,
+          quantity: product.quantity
+        };
+      }
+    }
+  }
+  newCartQuantityArray.push(newCart);
+  return newCartQuantityArray;
+}
+
+function getQuantity() {
+  let newQuantity;
+  for (index in quantity) {
+    newQuantity = quantity.item(index).value;
+  }
+  return newQuantity;
 }
