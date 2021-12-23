4f92a85a9964c5d410984ac04f1123c5dae9f682
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-23 23:02:56 +0100

    Added parameters in findContentStorage function and coded a condition to check if theses paramters are to be used + Added and coded storageSetTotalQuantity function + Added and coded setModifiedStorage function

--- front/js/storageFunctions.js
+++ front/js/storageFunctions.js
@@ -34,28 +34,63 @@ function isSetStorage(key) {
  * @param { Array } storageContent
  * @returns { Number } index number if found or -1 if nothing found
  */
-function findStorageContent(storageContent) {
-  var index = storageContent.findIndex(
-    (elements) =>
-      elements.id == productId && elements.color == productColors.value
-  );
+function findStorageContent(storageContent, IdOfProduct, productColor) {
+  console.log(IdOfProduct);
+  console.log(productColor);
+  if (IdOfProduct != null && productColor != null) {
+    var index = storageContent.findIndex(
+      (elements) => elements.id == IdOfProduct && elements.color == productColor
+    );
+    console.log(index);
+  } else {
+    var index = storageContent.findIndex(
+      (elements) =>
+        elements.id == productId && elements.color == productColors.value
+    );
+  }
   return index;
 }
 
 /**
  * Convert quantity strings to numbers and update it in the correct array entry
  * @param { Number } storageIndex index of the quantity entry that must be updated
- * @returns
+ * @returns { Array }
  */
 function modifyQuantity(storageIndex) {
   let currentCart = getLocalStorage('totalCart');
 
-  let currentQuantity = parseInt(currentCart[storageIndex].quantity);
-  let quantityToAdd = parseInt(productQuantity.value);
+  let currentQuantity = parseInt(currentCart[storageIndex].quantity),
+    quantityToAdd = parseInt(productQuantity.value);
 
   let newQuantity = currentQuantity + quantityToAdd;
 
   currentCart[storageIndex].quantity = newQuantity.toString();
 
   return currentCart;
+}
+
+/**
+ * Converts quantity string to Number and put this new value in the local stoarage Array
+ * @param { Array } storageContent Contains the current local storage content
+ * @param { Number } storageIndex Index in the localStorage array of the entry to modify
+ * @param { String } quantity Value of the new total quantity to set
+ * @returns { Array }
+ */
+function storageSetTotalQuantity(storageContent, storageIndex, quantity) {
+  let currentStorage = storageContent,
+    totalQuantityToSet = parseInt(quantity);
+
+  currentStorage[storageIndex].quantity = totalQuantityToSet.toString();
+
+  return currentStorage;
+}
+
+/**
+ * Removes actual localStorage and calls setLocalStorage function to set the new one
+ * @param { String } storageName name of the localStorage to replace
+ * @param { Array } arrayToReplaceBy Array containing the new products quantity that is going to replace the former one
+ */
+function setModifiedStorage(storageName, arrayToReplaceBy) {
+  localStorage.removeItem(storageName);
+  setLocalStorage(arrayToReplaceBy);
 }
