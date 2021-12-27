10bdc9f5a38075bd5d5a3c0f9c7672a9f9469e36
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-27 22:36:59 +0100

    Removed all the target.event if function and replaced it by function - and function call - runEvent + Amalgamated display message functions in only one + removed fetch function to appropriate file

--- front/js/form.js
+++ front/js/form.js
@@ -10,66 +10,24 @@ const firstNamePattern =
     /^[A-Z]{1}[a-z]+[îñôäëöü]{0,}[a-z]{0,}|[A-Z]{1}[a-z]+[\-]{1}[A-Z]{1}[a-z]+$/,
   lastNamePattern =
     /^[A-Z]{1}[a-z]+[îñôäëöü]{0,}|[A-Z]{1}[a-z]+[îñôäëöü]{0,}\s[A-Z]{1}[a-z]+ |[A-Z]{1}[a-z]+[\-][A-Z]{1}[a-z]+$/g,
+  addressPattern = /[0-9]{0,3}\s?[A-Za-zîñôäëöüÂÊÎÔÛÉÈÀÄËÏÖÜ]+/g,
+  cityPattern = /([A-Za-zîñôäëöüÂÊÎÔÛÉÈÀÄËÏÖÜ\-]{2,})+/g,
   emailPattern = /^([\.\_a-zA-Z0-9]+)@([a-z]+)\.([a-z]){2,3}$/;
 
 let errorMessage = '',
-  isError = false;
-let fieldNameValue = '';
+  isError = true;
 
 form.addEventListener('focusout', function (event) {
   let target = event.target;
-  if (event.target.getAttribute('id') == 'firstName') {
-    if (!checkFieldValueFormat(target.value, firstNamePattern)) {
-      isError = true;
-      displayErrorMessage(event.target.getAttribute('id'), 'prénom');
-    } else {
-      isError = false;
-      eraseErrorMessage(event.target.getAttribute('id'));
-    }
-  } else if (event.target.getAttribute('id') == 'lastName') {
-    if (!checkFieldValueFormat(target.value, lastNamePattern)) {
-      isError = true;
-      displayErrorMessage(event.target.getAttribute('id'), 'nom de famille');
-    } else {
-      isError = false;
-      eraseErrorMessage(event.target.getAttribute('id'));
-    }
-  } else if (event.target.getAttribute('id') == 'address') {
-    if (target.value == '') {
-      isError = true;
-      displayErrorMessage(event.target.getAttribute('id'), 'adresse');
-    } else {
-      isError = false;
-      eraseErrorMessage(event.target.getAttribute('id'));
-    }
-  } else if (event.target.getAttribute('id') == 'city') {
-    if (target.value == '') {
-      isError = true;
-      displayErrorMessage(event.target.getAttribute('id'), 'ville');
-    } else {
-      isError = false;
-      eraseErrorMessage(event.target.getAttribute('id'));
-    }
-  } else if (event.target.getAttribute('id') == 'email') {
-    if (!checkFieldValueFormat(target.value, emailPattern)) {
-      isError = true;
-      displayErrorMessage(event.target.getAttribute('id'), 'email');
-    } else {
-      isError = false;
-      eraseErrorMessage(event.target.getAttribute('id'));
-    }
-  }
+  var patternName = `${event.target.getAttribute('id')}Pattern`;
+  eval('var patternObj=' + patternName);
+
+  runEvent(target.getAttribute('id'), target.value, patternObj);
 });
 
 submit.addEventListener('click', function (event) {
   event.preventDefault();
-  if (
-    firstName.value != '' ||
-    lastName.value != '' ||
-    address.value != '' ||
-    city.value != '' ||
-    (email.value != '' && isError == false)
-  ) {
+  if (!isError && isSetStorage('totalCart')) {
     let contact = {
       contact: {
         firstName: firstName.value,
@@ -79,7 +37,6 @@ submit.addEventListener('click', function (event) {
         email: email.value
       }
     };
-
     var cart = getIdArray(cartContent);
 
     let cartProduct = {
@@ -94,47 +51,36 @@ submit.addEventListener('click', function (event) {
   }
 });
 
+function runEvent(idValue, fieldValue, fieldPattern) {
+  if (!checkFieldValueFormat(fieldValue, fieldPattern)) {
+    isError = true;
+    displayErrorMessage(idValue, isError);
+  } else {
+    isError = false;
+    displayErrorMessage(idValue, isError);
+  }
+}
+
 function checkFieldValueFormat(fieldValue, valuePattern) {
   var pattern = valuePattern;
 
   return fieldValue.match(pattern) ? true : false;
 }
 
-function displayErrorMessage(errorDiv, fieldNameValue) {
+function displayErrorMessage(errorDiv, isError) {
   fieldId = `${errorDiv}ErrorMsg`;
-  document.getElementById(
-    fieldId
-  ).innerText = `Merci de renseigner votre ${fieldNameValue}`;
-}
-
-function eraseErrorMessage(errorDiv) {
-  document.getElementById(`${errorDiv}ErrorMsg`).innerText = '';
+  if (isError) {
+    document.getElementById(fieldId).innerText = 'Merci de renseigner ce champ';
+  } else {
+    document.getElementById(`${errorDiv}ErrorMsg`).innerText = '';
+  }
 }
 
 function transformToJson(elementToTransform) {
   return JSON.stringify(elementToTransform);
 }
 
-function validateOrder(contentToPost) {
-  var json = JSON.stringify(contentToPost);
-  console.log(json);
-  fetch('http://localhost:3000/api/products/order', {
-    method: 'post',
-    body: json,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (jsonData) {
-      window.location.href = `./../html/confirmation.html?orderId=${jsonData.orderId}`;
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}
+
 
 function getIdArray(array) {
   let newArray = [];
